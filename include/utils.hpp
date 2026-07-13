a3a801e2436a125cf0f0dbd575bb1b4081fd8aad
IceWizard <mrrioes.dc@gmail.com>
2026-07-13 18:03:28 +0200

    Add Vector_Utils::contains()

--- include/utils.hpp
+++ include/utils.hpp
@@ -1,6 +1,7 @@
 #pragma once
 
 #include <string>
+#include <vector>
 
 using str = std::string;
 
@@ -9,3 +10,8 @@ str to_power_of10(double x);
 str round_to_hundreds(double x);
 
 str get_numerical_suffix(int x);
+
+namespace Vector_Utils {
+    template <typename T>
+    bool contains(T val, const std::vector<T> &vec);
+}
